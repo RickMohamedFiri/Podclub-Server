09b4c5ab3b71d8a3ed9b4ec455d5cb0a6086523a
Alex-gikungu <alexigikugnu.012@gmail.com>
2023-10-26 14:39:00 +0300

    imported app and db to the seed file

--- seed.py
+++ seed.py
@@ -1,5 +1,5 @@
-# from app import app, db
-# from models import User, Channel, Message, GroupMessage, ReportedUser, ReportedMessage, Invitation
+from app import app, db
+from models import User, Channel, Message, GroupMessage, ReportedUser, ReportedMessage, Invitation
 
 # def seed_database():
 #     with app.app_context():
