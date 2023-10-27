d0df12623aab98acb02e393b83c8a4d6c7e71a74
emmanuel <emmanuel.mukasa@moringaschool.com>
2023-10-27 13:11:25 +0300

    added user and signup route

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from config import Config
 from models import db
 from flask_migrate import Migrate
 from flask_restful import Api
-from flask_jwt_extended import JWTManager
-from datetime import timedelta
+from flask_jwt_extended import JWTManager,create_access_token
+from datetime import timedeltaz
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,10 +17,38 @@ jwt = JWTManager(app)
 
 # Configure JWT settings
 app.config['JWT_SECRET_KEY'] = 'your-secret-key'  
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)  # Token expiration time
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedeltaz(hours=1)  # Token expiration time
 
 # Import routes after JWT configuration
 from routes import *
+# User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+# User Sign Up
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({"message": "Username and password are required"}), 400
+
+    existing_user = User.query.filter_by(username=username).first()
+    if existing_user:
+        return jsonify({"message": "Username already exists"}), 400
+
+    new_user = User(username=username, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User signed up successfully"}), 201
+
 
 if __name__ == '__main':
+    db.create_all()
     app.run(debug=True)
+
+
